ea8e076b340622b535d2d3e07d78e8d836419157
putneydm <david@davidputney.com>
2014-09-07 19:53:10 -0400

    Creates functions for menu drop down on scroll. Open and close menu on mobile.

---
+++ scripts/functions.js
@@ -0,0 +1,88 @@
+$(document) .ready (function ()
+	{
+
+  console.log( "works" );
+
+$( "a" ).each( function(){
+  var href = $( this ).attr( "href" );
+  if( href.indexOf( "#" ) !== 0 ){
+                $( this ).bind( "tap", function(){
+                    window.location.href = this.href;
+                });
+            }
+} );
+
+
+
+
+
+$('#menu-button' ).bind( "tap", function( e ){ 
+ 
+var menu_height = $('#secondary-header') .height ();
+	console.log(menu_height);
+
+ if (menu_height == 40)
+	 {
+	 $('#secondary-header') .addClass ('menu-active');
+	 $('.button_hidden') .show ();
+	 $('.button_visible') .hide ();
+	}
+	
+ else
+	 {
+	 $('#secondary-header') .removeClass ('menu-active');
+	 $('.button_hidden') .hide ();
+	 $('.button_visible') .show ();
+	}	
+
+}); 
+
+
+}); 
+
+
+$(window) .resize (function ()
+	{
+	var window_width = $(window) .width ();
+	var menu_height = $('#secondary-header') .height ();
+	
+	console.log(menu_height);
+	
+	$('#secondary-header') .removeClass ('menu-active');
+	$('.button_hidden') .hide ();
+	$('.button_visible') .show ();
+
+	}); 
+
+
+
+
+$(window) .scroll (function ()
+	{
+	var window_width = $(window) .width ();	
+	scroll_position = $(document) .scrollTop ();
+    main_position = $('#main') .offset ();
+    
+  function sticky_menu (main_position, scroll_position)
+		{			
+		if (main_position < scroll_position)
+		{
+		$('#secondary-header') .addClass ('social-menu-show');	
+		}
+		
+		if (main_position > scroll_position)
+		{
+		$('#secondary-header') .removeClass ('social-menu-show') .removeClass ('menu-active');
+		$('.button_hidden') .hide ();
+		$('.button_visible') .show ();
+		}		
+		}		  
+    
+	
+   $.data(this, 'scrollTimer', setTimeout(function() {    
+		sticky_menu(main_position.top, scroll_position);		
+ }, 50));
+
+	}); 
+
+
